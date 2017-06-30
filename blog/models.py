1c6cc81cb29980e6d5652c26acb74e1f431eb205
arrnwo <arriana1.nwodu@famu.edu>
2017-06-30 11:44:58 -0400

    added css file in static folder

--- blog/models.py
+++ blog/models.py
@@ -7,10 +7,12 @@ class Post(models.Model):
 	title = models.CharField(max_length=200)
 	text = models.TextField()
 	created_date = models.DateTimeField(blank=True, null=True)
-	
+	published_date = models.DateTimeField(
+            blank=True, null=True)
+
 	def publish(self):
 		self.published_date = timezone.now()
 		self.save()
-		
+
 	def __str__(self):
 		return self.title
