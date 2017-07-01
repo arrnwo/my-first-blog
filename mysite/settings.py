21df5056b68a8c313e186a8653390ce9dd674516
arrnwo <arriana1.nwodu@famu.edu>
2017-07-01 19:36:42 -0400

    please dear god dont mess this up.  Ahhem I mean changing the local files so it can appear when i runserver

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import os.path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -117,6 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = os.path.join(os.path.join('static'))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
